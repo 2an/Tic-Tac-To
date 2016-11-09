6aa2365e5a5e9ac67df1efc2636dbdcbfc1829fe
Tuan Ngo <Tuan@macbookpromain.fritz.box>
2016-11-09 20:02:57 +0100

    Initial Commit

---
+++ Tic Tac To/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Tic Tac To
+//
+//  Created by Tuan Ngo on 09.11.16.
+//  Copyright © 2016 Tuan Ngo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
